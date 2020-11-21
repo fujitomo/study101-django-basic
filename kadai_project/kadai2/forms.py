5e642657cf4519c2f256f0e1369cdfc48288154e
FUJITOMO375 <fujitomo375@gmail.com>
2020-11-21 21:46:33 +0900

    base.html修正とFormクラス修正（ラベル値等）

--- kadai_project/kadai2/forms.py
+++ kadai_project/kadai2/forms.py
@@ -1,13 +1,21 @@
 from django import forms
-from .models import KimetsuCharactorModel
+from .models import KimetsuCharactorModel, SexModel
 
 
 class KimetsuCharactorForm(forms.ModelForm):
     """鬼滅の刃キャラクターForm用"""
+    name = forms.CharField(
+        label='名前', max_length=30
+    )
+
+    sex_id = forms.ModelChoiceField(
+        SexModel.objects, label='性別'
+    )
+
+    characteristic = forms.CharField(
+        label='特徴', max_length=300, widget=forms.Textarea
+    )
+
     class Meta:
         model = KimetsuCharactorModel
-        fields = ("name", "sex_id", "characteristic")
-        widgets = {
-            'characteristic': forms.Textarea
-
-        }
+        fields = ()  # TODO : このコードがないとエラーが発生する
